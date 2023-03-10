4949d7de913f61d2d2e561276bb47d7634b06e1b
AmondChocolateShake <dongju230130@gmail.com>
2023-03-10 17:23:28 +0900

    test

---
+++ source/modulefile.js
@@ -0,0 +1,237 @@
+
+
+const _EXAMDATA = {
+  teamInformation: {
+    teamNumber : 0,
+    teamName: "세동서유",
+    teamNameStory: "세종 동구 서구 유성구",
+    teamManager: "김동주",
+    teamAllMember: [
+      {
+        memberNumber: "010-0000-0000",
+        name: "김동주",
+        teamPosition: "팀장",
+        githubAddress: "https://github.com/AmondChocolateShake"
+      },
+      {
+        memberNumber: "010-0000-0000",
+        name: "임지성",
+        teamPosition: "대리",
+        githubAddress: "https://github.com/WJKK21"
+      },
+      {
+        memberNumber: "010-0000-0000",
+        name: "이재권",
+        teamPosition: "과장",
+        githubAddress: "https://github.com/nowkwon97"
+      },
+      {
+        memberNumber: "010-0000-0000",
+        name: "최대건",
+        teamPosition: "부장",
+        githubAddress: "https://github.com/dgchoi3904"
+      },
+      {
+        memberNumber: "010-0000-0000",
+        name: "정지은",
+        teamPosition: "실장",
+        githubAddress: "https://github.com/jiny43"
+      },
+    ]
+  },
+  trainingInformation: {
+    subject :  
+    "웹프로그래밍기본(HTML5&CSS3),Git&GitHub,Babel Javascript,프레임워크 기초,Project C(선택),백엔드프레임워크런타임,프레임워크활용,Project A(데이터 기반 주식 추천 서비스),Project B(교통정보제공서비스)"
+    
+  },
+  designInformation: {
+    colorSet : ["cadetblue", "salmon", "#ccc", "#333"],
+    breakPoint : [ "768px", "1280px",  "1920px"]
+  }
+}
+
+
+const _INIT={
+  display:"flex",
+  width:"100vw",
+  height:"100vh",
+  minHeight:"700px",
+  minWidth:"850px",
+  flexDirection:"column",
+  justifyContent:"center",
+  alignItems:"center",
+}
+
+const _BOXINIT={
+  display:"flex",
+  width:"70%",
+  height:"70%",
+  flexDirection:"column",
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor:"#330643",
+  innerBox:{
+    display:"flex",
+    width:"70%",
+    height:"50%",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    
+
+
+  }
+}
+
+const _LOGO={
+  logoPath:"./source/logo.png",
+  slogan:"",
+  backgroundColor:"",
+  fontColor:"",
+  
+}
+
+const _SLOGAN={
+  color:"#FCFF6B",
+  fontSize:"100px",
+  fontFamily:"Anton,sans-serif",
+}
+const _SLOGANTEXT={
+  color:"#FCFF6B",
+  fontSize:"50px",
+
+
+}
+
+const _SMALLTXT={
+  color:"#FCFF6B",
+  fontSize:"20px",
+
+}
+
+
+const _PROJECT_PROFILE={
+  backgroundColor:"grey"
+
+}
+
+const _PROJECTBOXINIT={
+  display:"flex",
+  width:"70%",
+  height:"70%",
+  flexDirection:"column",
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor:"#330643",
+  innerBox:{
+    display:"flex",
+    width:"80%",
+    height:"10%",
+    justifyContent:"center",
+    alignItems:"center",
+    
+
+
+  }
+}
+
+
+const _TEAMBOX={
+  display:"flex",
+  width:"70%",
+  height:"70%",
+  flexDirection:"column",
+  justifyContent:"space-around",
+  alignItems:"center",
+  backgroundColor:"#330643",
+  innerBox:{
+    display:"flex",
+    width:"100px",
+    height:"100px",
+    justifyContent:"center",
+    alignItems:"center",
+    backgroundColor:"#FCFF6B",
+    borderRadius:"50%",
+    
+  }
+}
+
+
+const _PBOX={
+  display:"flex",
+  width:"80%",
+  height:"70%",
+  justifyContent:"space-around",
+  flexDirection:"column",
+  color:"#FFFFFF",
+  fontWeight:"bold",
+}
+
+const _PROFILE={
+  width:"100%",
+  height:"50px",
+
+}
+
+
+
+const _SUB={
+  display:"flex",
+  width:"70%",
+  height:"70%",
+  flexDirection:"column",
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor:"#330643",
+  color:"#FCFF6B",
+  innerBox:{
+    display:"flex",
+    width:"70%",
+    height:"20%",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    
+
+
+  },
+  innerBox2:{
+    width:"70%"
+  }
+}
+
+
+
+
+
+const _CONTACT={
+  display:"flex",
+  width:"70%",
+  height:"70%",
+  flexDirection:"column",
+  justifyContent:"center",
+  alignItems:"center",
+  backgroundColor:"#330643",
+  color:"#FCFF6B",
+  fontSize:"100px",
+  fontFamily:"Anton,sans-serif",
+  innerBox:{
+    display:"flex",
+    width:"70%",
+    height:"30%",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    
+
+
+  }
+}
+
+const _CONTACT_TXT={
+  color:"#FCFF6B",
+  fontSize:"20px",
+  fontFamily:"Anton,sans-serif",
+
+
+}
